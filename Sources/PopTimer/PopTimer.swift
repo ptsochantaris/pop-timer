18ca7cdc25b78345f6a7d3a0b1dee735612462f7
Paul Tsochantaris <ptsochantaris@icloud.com>
2023-08-18 12:34:22 +0100

    `public` modifiers

--- Sources/PopTimer/PopTimer.swift
+++ Sources/PopTimer/PopTimer.swift
@@ -2,22 +2,22 @@ import Combine
 import Foundation
 
 // An ultra-simple debouncing push-button / toaster / egg-timer style timer which starts when pushed and fires its callback when it is done. Pushing it again restarts it.
-final class PopTimer {
+public final class PopTimer {
     private let publisher = CurrentValueSubject<Bool, Never>(false)
     private var observation: Cancellable?
 
     /// Start the timer. If the timer is already running, reset the time interval.
-    func push() {
+    public func push() {
         publisher.send(true)
     }
 
     /// Abort the timer so that it doesn't fire the callback when done.
-    func abort() {
+    public func abort() {
         publisher.send(false)
     }
 
     /// Has the timer been pushed already (i.e. is it ticking?)
-    var isPushed: Bool {
+    public var isPushed: Bool {
         publisher.value
     }
     
@@ -25,7 +25,7 @@ final class PopTimer {
     /// - Parameters:
     ///   - timeInterval: The time interval that the timer needs to wait since the most recent push to fire the callback
     ///   - callback: The code to run when the timer is done. This can be an async block. The block will run in the thread in which the timer was created unless otherwise specified (for instance, with @MainActor).
-    init(timeInterval: TimeInterval, callback: @escaping () async -> Void) {
+    public init(timeInterval: TimeInterval, callback: @escaping () async -> Void) {
         let stride = RunLoop.SchedulerTimeType.Stride(timeInterval)
         observation = publisher.debounce(for: stride, scheduler: RunLoop.current).sink { value in
             guard value else { return }
